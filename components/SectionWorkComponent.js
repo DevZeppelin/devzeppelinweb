81b04995737749f797c371f6da3480ed1b3b5100
DevZeppelin <gino.b.pietrobon@gmail.com>
2021-10-04 14:26:44 -0300

    another changes in sectionWorkComponent

--- components/SectionWorkComponent.js
+++ components/SectionWorkComponent.js
@@ -8,18 +8,18 @@ const SectionWorkComponent = ({description, web, src}) => {
         alt="responsive image mobile tablet notebook widescreen responsive-design"
         className="w-full  animateImage"
       />
-      <p className="text-lg text-yellow text-center pt-3 md:pt-8 uppercase font-bold">
+      <p className="text-lg text-yellow text-center pt-7 md:pt-8 uppercase font-bold">
         {description}
       </p>
       <a href={`https://${web}`} rel="noopener" target="_blank">
-          <p className="text-base text-lightGray text-center pt-3 md:pt-4 font-bold">
+          <p className="text-base text-lightGray text-center pt-2 md:pt-3 font-bold">
             {web}
           </p>
       </a>
       <style jsx>{` 
       
       .animateImage:hover {
-          transform: scale(1.2);
+          transform: scale(1.15);
           padding: 0;
           transition: 1.5s
       }
