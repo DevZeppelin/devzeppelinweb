efcf74021fa2e7df779f506c94002ebf59c3c0ed
DevZeppelin <gino.b.pietrobon@gmail.com>
2021-10-06 17:36:35 -0300

    cahnge ubication to section work component

--- components/sections/SectionWorks.js
+++ components/sections/SectionWorks.js
@@ -1,5 +1,5 @@
 import Fade from "react-reveal/Fade";
-import SectionWorkComponent from "../SectionWorkComponent";
+import SectionWorkComponent from "./SectionWorkComponent";
 
 const SectionWorks = () => {
   return (
