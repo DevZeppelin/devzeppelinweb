e02a8f1567ff6cefcce626ac8737d115e26dca86
Gino Pietrobon <gino.b.pietrobon@gmail.com>
2024-09-12 01:08:22 -0300

    try again repair all my stupid merge errors, sniff

--- components/sections/SectionBackgroundFixed.js
+++ components/sections/SectionBackgroundFixed.js
@@ -7,25 +7,21 @@ const SectionBackgroundFixed = () => {
         <div className="p-8 md:p-32 py-32 font-bold text-center bg-cover bg-hero-bigger bg-right bg-fixed grid grid-cols-1 md:grid-cols-2">
           <div className="space-y-6">
             <h2 className="text-3xl font-righteous fadeIn text-yellow textDark">
-              ¿Has invertido en tu marca?
+            Inyectá tecnología a tu marca
             </h2>
             <div className="text-primary space-y-3 text-lg textLight font-bold">
               <p>
-                <strong>Dev Zeppelin</strong> te acompaña en tu incersión al
-                nuevo mundo de internet.
+              El <strong>propósito</strong> es que tu marca sea visible, y que vos no te preocupes por nada.
               </p>
               <p>
-                Estoy seguro que toda tu inversión en tu marca serà bien retribuìda en ventas y nuevos clientes.
-              </p>             
-              <p>
-               Que cuando busquen tu rubro aparezca tu negocio. Que tu Instagram marque la diferencia. Que tus logos, fotos y afiches demuestren que te importa la presencia de tu marca.
+              Presencia en las redes, imagen moderna y asesoramiento para encontrar la estrategia de propaganda adecuada. 
               </p>
             </div>
             <div></div>
 
             <Fade big>
               <Link href="/contacto">
-                <button className="btn uppercase">¡Reunámonos!</button>
+                <button className="btn uppercase">Llamame</button>
               </Link>
             </Fade>
           </div>
--- components/sections/SectionFirstFlayer.js
+++ components/sections/SectionFirstFlayer.js
@@ -3,10 +3,10 @@ const SectionFirstFlayer = () => {
     <section>
       <div className="space-y-2 text-center p-4 md:p-12 px-8 md:px-32 pt-8 bg-yellow">
         <h1 className="font-bold text-lg md:text-2xl tracking-wider text-secundary uppercase">
-          Soluciones Tecnológicas
+          Seamos lo mejor para nuestros clientes
         </h1>
         <p className="text-secundary md:text-lg">
-          ¡Digitalizate! Modernizá tus fotos, tus colores y tus ideas. Hace de la tecnología tu aliada para ganar clientes y crecer en el nuevo mundo.
+        Subite al tren del futuro. Tecnología + IA + Ideas + Estrategia de marketing
         </p>
       </div>
     </section>
